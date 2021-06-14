6bd0e1bd2f8b08a5fb6adcfff4bbab382028ce38
Paul Barclay <paul@dragonfoundry.com>
2021-06-14 10:30:11 -0700

    Add mumbai addresses to deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,16 +6,39 @@ const FacetCutAction = {
   Remove: 2
 }
 
+const isMumbai = true;
+
+// Addresses for Matic Mumbai contracts
+const mumbai_gateway = "0xb5505a6d998549090530911180f38aC5130101c6"; // used for deploying testtokens
+const mumbai_wmatic = "0xd8b90bb9253980967dE87A23009e56E6CC8B7cE6"; // Used for deploying uniswap router
+const mumbai_uniswap_factory = "0x5418dc4506481407FdfcAdE334374DF4fC221c23";
+const mumbai_uniswap_router = "0x1e860A6870ae89Ecd74a0fdF8EeF3b606946f339";
+const mumbai_game_erc20 = "0xa1FB4a6474661114cf865B062B7C669d936b0896";
+const mumbai_game_game = "0xf6c213D3076bc22dAC297Bbad7A6d134ebea8025";
+const mumbai_game_master = "0x9f84EaeA0155A17C5eea7F3579ac4783a51E7B14";
+const mumbai_fee_receiver = mumbai_game_erc20; // having the erc20 as the fee receiver allows our cfo role to withdraw funds
+const mumbai_quick_testtoken = "0x7aD20222E855CD854B15364f55896599eBbC769B";
+const mumbai_usdc_testtoken = "0xA0fb4A353B60B6Ef6ef9BA87F4B09680605a7932";
+const mumbai_usdt_testtoken = "0xD7De69D7e1ecf87f8dE45E017f7C9fDf24F14d8E";
+
+const mumbai_addresses = [mumbai_game_erc20, mumbai_game_game, mumbai_game_master, mumbai_fee_receiver];
+
+// Addresses for testnet
+const mainnet_uniswap_router = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
+const mainnet_game_erc20 = "0x8d1566569d5b695d44a9a234540f68D393cDC40D"
+const mainnet_game_game =  "0x1d01438aa932e95e31b2585e3e27b855e6f380f7"
+const mainnet_game_master = "0x2550aa5c84edb92a66125a85527c151923be35e1"
+const mainnet_fee_receiver = "0x0"
+const mainnet_addresses = [mainnet_game_erc20, mainnet_game_game, mainnet_game_master, mainnet_fee_receiver];
+
+// Select addresses based on target network
+const maticAddresses = isMumbai ? mumbai_addresses : mainnet_addresses;
+const routerAddress = isMumbai ? mumbai_uniswap_router : mainnet_uniswap_router;
+
 function getSelectors (contract) {
   const selectors = Object.keys(contract.interface.functions).map(v => ethers.utils.id(v).slice(0, 10));
   return selectors
 }
-
-const uniswapRouter = "0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff"
-const gameToken = "0x8d1566569d5b695d44a9a234540f68D393cDC40D"
-const gameAdmin =  "0x1d01438aa932e95e31b2585e3e27b855e6f380f7"
-const gameMaster = "0x2550aa5c84edb92a66125a85527c151923be35e1"
-const feeReceiver = "0x0"
 
 async function main() {
   const WorkerMetaTransactions = await ethers.getContractFactory("WorkerMetaTransactions");
@@ -27,9 +50,9 @@ async function main() {
     [workerMetaTransactions.address, FacetCutAction.Add, getSelectors(workerMetaTransactions)]
   ]
   const BondingSale = await ethers.getContractFactory("BondingSale");
-  const bondingSale = await BondingSale.deploy(diamondCut, [gameToken, gameAdmin, gameMaster, feeReceiver]);
+  const bondingSale = await BondingSale.deploy(diamondCut, maticAddresses);
   await bondingSale.deployed();
-  await bondingSale.setUniswapRouter(uniswapRouter);
+  await bondingSale.setUniswapRouter(routerAddress);
   console.log("BondingSale address:", bondingSale.address);
 }
 
